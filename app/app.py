f6d2ad0b31669de75564e3bfff143bdca71e4fcd
fbc2 <felipeb.castro02@gmail.com>
2025-05-10 05:44:26 -0300

    App.py modify

--- app/app.py
+++ app/app.py
@@ -1,34 +1,39 @@
 from flask import Flask, request, jsonify
 from validator.engine import validate
 from pymongo import MongoClient
-import os
 from datetime import datetime
 
 app = Flask(__name__)
 
-# MongoDB URI (ajustado ao serviço do Docker)
+# Conexão com o MongoDB
 client = MongoClient("mongodb://mongo:27017/")
 db = client["hackathon"]
-collection = db["avaliacoes"]
 
 @app.route("/validate", methods=["POST"])
 def validar():
     data = request.get_json()
     is_valid, messages = validate(data)
-
-    # Armazena no Mongo
-    collection.insert_one({
+    db.validacao.insert_one({
         "input": data,
         "valid": is_valid,
         "messages": messages,
         "timestamp": datetime.utcnow()
     })
-
     return jsonify({"valid": is_valid, "messages": messages})
 
 @app.route("/alimentos", methods=["GET"])
-def listar_alimentos():
-    registros = list(collection.find({}, {"_id": 0}))
+def listar_validacoes():
+    registros = list(db.validacao.find({}, {"_id": 0}))
+    return jsonify(registros)
+
+@app.route("/empresas", methods=["GET"])
+def listar_empresas():
+    registros = list(db.empresas.find({}, {"_id": 0}))
+    return jsonify(registros)
+
+@app.route("/produtos", methods=["GET"])
+def listar_produtos():
+    registros = list(db.produtos.find({}, {"_id": 0}))
     return jsonify(registros)
 
 if __name__ == "__main__":
