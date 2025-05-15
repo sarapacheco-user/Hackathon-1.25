46f94ecf7a5d64c1ae34570f6bea76f765828c4d
Sara Pacheco de Azevedo <thesarapacheco@gmail.com>
2025-05-15 13:21:14 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 pip install -r requirements.txt
 
-# Subir os containers do Docker
+# To Build Docker files
 
 docker compose up -d --build
 
-# Criar o arquivo da API KEY
+# To Create the API Key
 
 echo "GEMINI_API_KEY=yourapikey" > .env
 
-# Rodar o frontend do sistema
+# To Run the appliction
 
 streamlit run ./frontend/home.py
