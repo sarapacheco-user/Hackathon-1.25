06fe38df65576671b8ff1a8ceef27ea3096e4b3a
Sara Pacheco de Azevedo <thesarapacheco@gmail.com>
2025-05-15 13:34:25 -0300

    Update dashboard.py

--- frontend/pages/dashboard.py
+++ frontend/pages/dashboard.py
@@ -10,16 +10,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Caminhos
+# Paths
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 infoProdutos = "./database/Produtos.csv"
 
 st.set_page_config(page_title="dashboard", layout="centered")
-st.title("Análise Estatística")
+st.title("Statistic Analysis")
 
 with st.sidebar:
     st.markdown(
-        '[![Open in GitHub](https://github.com/codespaces/badge.svg)](https://github.com/Felipebc2/Hackathon-1.25)',
+        '[![Open in GitHub](https://github.com/codespaces/badge.svg)](https://github.com/sarapacheco-user/Hackathon-1.25)',
         unsafe_allow_html=True
     )
     st.image("frontend/shareBiteLogo.png", caption=" ", use_container_width=True)
@@ -28,29 +28,29 @@ try:
     df = pd.read_csv(infoProdutos)
 
     if 'produto' in df.columns and 'quantidade' in df.columns:
-        st.subheader("Quantidade Produto Excedido")
+        st.subheader("Amount of Exceded Product")
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.bar(df['produto'], df['quantidade'], color='skyblue')
-        ax.set_title("Quantidade por Produto")
-        ax.set_xlabel("Produto")
-        ax.set_ylabel("Quantidade")
+        ax.set_title("Amount of Product")
+        ax.set_xlabel("Product")
+        ax.set_ylabel("Amount")
         ax.set_xticklabels(df['produto'], rotation=45)
         st.pyplot(fig)
     else:
-        st.warning("Colunas 'produto' e/ou 'quantidade' não encontradas.")
+        st.warning("Rows 'produto' and/or 'quantidade' not found")
 
     if 'valido' in df.columns:
-        st.subheader("Status das Doações")
+        st.subheader("Donation Status")
         contagem = df['valido'].value_counts()
-        contagem.index = contagem.index.map({True: "Foi doado", False: "Não foi doado"})
+        contagem.index = contagem.index.map({True: "Donated", False: "Not donated"})
 
         fig2, ax2 = plt.subplots()
         contagem.plot(kind='bar', color=['green', 'red'], ax=ax2)
-        ax2.set_title("Doações Realizadas")
+        ax2.set_title("Donations made")
         ax2.set_xticklabels(contagem.index, rotation=0)
         st.pyplot(fig2)
     else:
-        st.warning("Coluna 'valido' não encontrada.")
+        st.warning("Row 'valido'not found.")
 
 except FileNotFoundError:
-    st.error("Arquivo de produtos não encontrado.")
+    st.error("Product file was not found")
